26201822ee0e80b45525e5d5b4f9613d81834720
sherkhan7 <9669470@gmail.com>
2020-11-24 09:48:17 +0500

    test sendMessage to the user

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
 from flask import Flask, jsonify, url_for, request
+from database import get_user_attempt
+import requests
+from config import full_url
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/api', methods=['GET', 'POST'])
-def hello_world():
-    data = request.get_json()
-
+def api_callback():
     if request.method == 'POST':
-        print('data: ', data)
+        data = request.get_json()
+        res = send_message(data)
 
-        return {'status': 200}
+        return jsonify({'ok': res})
+
+
+def send_message(data):
+    attempt_id = data['id']
+    result = data['result']
+    reply_text = json.dumps(result, indent=2)
+    reply_text = '<pre>' + reply_text + '</pre>'
+
+    # attempt = get_user_attempt(attempt_id)
+    attempt = dict()
+    attempt['user_tg_id'] = 197256155
+    method = '/sendMessage'
+    url = full_url + method
+    params = {'chat_id': attempt['user_tg_id'], 'text': reply_text, 'parse_mode': 'HTML'}
+    r = requests.get(url, params=params)
+
+    return r.json()['ok']
 
 
 if __name__ == '__main__':
---
+++ database/main.py
@@ -0,0 +1,44 @@
+import pymysql.cursors
+from contextlib import closing
+import json
+from config import DB_CONFIG
+from pprint import pprint
+
+
+def get_connection():
+    connection = pymysql.connect(
+        host=DB_CONFIG['host'],
+        user=DB_CONFIG['user'],
+        password=DB_CONFIG['password'],
+        database=DB_CONFIG['database'],
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+    return connection
+
+
+users_table_name = 'robocontest_bot_users'
+attempts_table_name = 'robocontest_bot_attempts'
+
+
+def get_user_attempt(attempt_id):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(f'SELECT * FROM testdb.{attempts_table_name} WHERE attempt_id = %s', id)
+            record = cursor.fetchone()
+
+    return record
+
+
+def update_attempt_status(id, status):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
+            sql = f'UPDATE testdb.{attempts_table_name} SET status = %s WHERE attempt_id = %s'
+            cursor.execute(sql, (status, id))
+            connection.commit()
+
+    return_value = 'not updated'
+
+    if connection.affected_rows() != 0:
+        return_value = 'updated'
+
+    return return_value
